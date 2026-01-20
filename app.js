2dc56b58068ddcdb1207c971fc20b6235fb88818
Khonshu <amitbajracharya4444@gmail.com>
2026-01-20 13:45:54 +0545

    added database connectivity

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const app  = express ()
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-app.listen(5000, ()=>{
-    console.log("app is listening to port 5000")
+app.use(express.urlencoded({extended: false}))
+
+
+app.get('/', (req, res)=>{
+    res.send("This is homepage")
+})
+
+mongoose.connect(
+process.env.DATABASE_URL    
+).
+then(()=>{
+    console.log("Connected to Database")
+    app.listen(5000, ()=>{
+        console.log("app is listening to port 5000")
+    })
+}).catch((err)=>{
+    console.log("Unable to Connect to database")
 })
