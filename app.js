6c995e438b94bdea445c7ebcbfc70bd765067cea
Khonshu <amitbajracharya4444@gmail.com>
2026-01-20 14:33:55 +0545

    Added add user and get user functionality

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app  = express ()
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+const user = require('./models/user_schema.js')
+
 //TO GET DATA IN JSON FILE FORMAT
 app.use(express.json())
 
@@ -14,8 +16,32 @@ app.get('/', (req, res)=>{
     res.send("This is homepage")
 })
 
-//
+//ADD NEW USER 
+app.post('/user/api', async (req, res)=>{
+   try {
+     const adduser = await user.create(req.body)
+     if(!adduser){
+         return res.status(400).json({successfull: false, data: "Unable To add User "})
 
+     }
+     return res.status(200).json({succesfull: true, data: adduser})
+   } catch (error) {
+    res.status(400).json({succesfull: false, data: error.getMessage})
+   }
+})
+
+//GET ALL USER
+app.get('/user/api', async(req, res)=>{
+    try{
+        const getUser = await user.find({})
+        if(!getUser){
+            return res.status(400).json({successfull: false, data: "Unable To get User "})
+        }
+        return res.status(200).json({successfull: true, data: getUser})
+    }catch(error){
+        res.status(400).json({succesfull: false, data: error.getMessage})
+    }
+})
 mongoose.connect(
 process.env.DATABASE_URL    
 ).
--- models/user_schema.js
+++ models/user_schema.js
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-const User = mongoose.model('User', userSchema)
+const user = mongoose.model('User', userSchema)
 
 
 module.exports = user;
